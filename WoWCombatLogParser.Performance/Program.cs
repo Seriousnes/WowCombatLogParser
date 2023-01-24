eee07a0ae487b39c876281056d03910dfa31311f
Sean Smith <ranky26@gmail.com>
2023-01-25 08:54:15 +1000

    Removed redudant projects

--- WoWCombatLogParser.Performance/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using WoWCombatLogParser;
-
-var context = new ApplicationContext();
-context.CombatLogParser.Filename = @"C:\Users\Sean\source\repos\WoWCombatLogParser\WoWCombatLogParser.Tests\TestLogs\SingleFightCombatLog.txt";
-var encounters = context.CombatLogParser.Scan().ToList();
-await context.CombatLogParser.ParseAsync(encounters);
